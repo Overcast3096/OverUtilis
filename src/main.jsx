3a5d60f0171778f5d795ee46efb2affd34901207
Overcast <0vercastsea6114@gmail.com>
2026-01-12 01:49:24 -0800

    react router theoretical routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,42 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
+import { createBrowserRouter, RouterProvider } from "react-router";
+
 import App from "./App.jsx";
+import Main from "./components/Main.jsx";
+import Calculator from "./components/catagories/Calculator.jsx";
+import Converter from "./components/catagories/Converter.jsx";
+import Improver from "./components/catagories/Improver.jsx";
+import AlphaNumber from "./components/catagories/modules/AlphaNumber.jsx";
+import Waterline from "./components/catagories/modules/Waterline.jsx";
+
 import "./styles/reset.css";
 import "./index.css";
 
+const router = createBrowserRouter([
+    {
+        path: "/",
+        Component: App,
+        children: [
+            { path: "converters", Component: Converter },
+            {
+                path: "calculators",
+                Component: Calculator,
+                children: [{ path: "waterline", Component: Waterline }],
+            },
+            { index: true, Component: Calculator },
+            {
+                path: "improvers",
+                Component: Improver,
+                children: [{ path: "a1z26", Component: AlphaNumber }],
+            },
+        ],
+    },
+]);
+
 createRoot(document.getElementById("root")).render(
     <StrictMode>
-        <App />
+        <RouterProvider router={router} />
     </StrictMode>
 );
